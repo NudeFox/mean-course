7e2b19571a6c5ce03fdb06190ff991163f6667a5
Oleksandr Ismagulov <gendalfwar@gmail.com>
2024-09-24 18:13:52 +0200

    - Deployment updates;

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,8 @@ mongoose
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join("backend/images")));
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use("/", express.static(path.join(__dirname, "angular")));
 
 app.get("/favicon.ico", (req, res) => res.status(204));
 
@@ -40,5 +41,8 @@ app.use((req, res, next) => {
 
 app.use("/api/posts", postsRoutes);
 app.use("/api/user", userRoutes);
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "angular", "index.html"));
+});
 
 module.exports = app;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,1 +1,5 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  apiUrl:
+    'http://my-nodejs-app-env.eba-h4qtd3g7.eu-north-1.elasticbeanstalk.com/api',
+};
